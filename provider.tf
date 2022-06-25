11f3b87a0a862847c5fcd1a350305454d189c02f
AJITHKP05 <ajithkp1537@gmail.com>
2022-06-25 14:53:10 -0400

    aaa

--- provider.tf
+++ provider.tf
@@ -1,14 +1,9 @@
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.74.0"
-    }
-  }
-}
+# provider.tf
+
 # Specify the provider and access details
 provider "aws" {
   profile                 = "demo"
   region                  = var.aws_region
+  
 }
