ddcb7a69b221b46629f02f62c94b7afd71910d55
AJITHKP05 <ajithkp1537@gmail.com>
2022-06-25 14:45:33 -0400

    aaa

--- provider.tf
+++ provider.tf
@@ -9,7 +9,6 @@ terraform {
 }
 # Specify the provider and access details
 provider "aws" {
-  shared_credentials_file = "~/.aws/credentials"
   profile                 = "demo"
   region                  = var.aws_region
 }
