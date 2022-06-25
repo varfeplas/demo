b6e9c60193b0fce1c8edad932c9fa07a79133319
AJITHKP05 <ajithkp1537@gmail.com>
2022-06-25 14:38:59 -0400

    aaa

--- provider.tf
+++ provider.tf
@@ -1,6 +1,12 @@
 
-# provider.tf
-
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.74.0"
+    }
+  }
+}
 # Specify the provider and access details
 provider "aws" {
   shared_credentials_file = "~/.aws/credentials"
